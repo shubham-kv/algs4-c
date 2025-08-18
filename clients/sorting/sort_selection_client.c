0d792b5aa75ccb8902bced6ad1f01f6b61764a52
Shubham Vishwakarma <90132246+shubham-kv@users.noreply.github.com>
2025-08-18 23:16:37 +0530

    Move sort_selection files to /lib & /clients and add cmake configurations

--- clients/sorting/sort_selection_client.c
+++ clients/sorting/sort_selection_client.c
@@ -8,12 +8,12 @@ int charCmp(const void *a, const void *b);
 void printChars(char *str, size_t n);
 
 /**
- * ### Compile:
- * `% mkdir build`
- * `% gcc src/sort_selection_client.c src/sort_selection.c -o build/sort_selection_client.out`
+ * ### Compile
+ * % cmake -S . -B build
+ * % make -C build
  *
- * ### Run:
- * `% build/sort_selection_client.out`
+ * ### Run
+ * % ./build/bin/sort_selection_client
  *
  */
 int main() {
---
+++ include/comparators.h
@@ -0,0 +1,7 @@
+#ifndef __comparators_h
+#define __comparators_h
+
+typedef int (*ComparatorFn)(const void *a, const void *b);
+
+#endif // __comparators_h
+
