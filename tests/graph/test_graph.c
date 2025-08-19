f69134b85a138024f0270bad420db8298816404e
Shubham Vishwakarma <90132246+shubham-kv@users.noreply.github.com>
2025-08-19 20:34:17 +0530

    Configure tests & test for graph (adjacency lists) data structure

---
+++ tests/graph/test_graph.c
@@ -0,0 +1,68 @@
+#include <criterion/criterion.h>
+#include <errno.h>
+#include "graph.h"
+
+static Graph graph;
+static const int VERTICES = 13;
+
+static void setup() {
+  graph = Graph_Create(VERTICES);
+}
+
+static void teardown() {
+  Graph_Free(&graph);
+}
+
+Test(
+    graph_vertices,
+    returns_correct_number_of_vertices,
+    .init = setup,
+    .fini = teardown
+) {
+  int v = 0, code;
+  code = Graph_Vertices(graph, &v);
+  cr_assert(v == VERTICES, "Expected %d vertices got %d", VERTICES, v);
+  cr_assert(code == 0);
+}
+
+Test(
+    graph_edges,
+    returns_correct_number_of_edges,
+    .init = setup,
+    .fini = teardown
+) {
+  int expectedEdges = 0;
+  Graph_AddEdge(graph, 0, 1), (expectedEdges++);
+  Graph_AddEdge(graph, 1, 2), (expectedEdges++);
+
+  int edges = 0, code;
+  code = Graph_Edges(graph, &edges);
+  cr_assert(edges == expectedEdges, "Expected %d edge(s) got %d", expectedEdges, edges);
+  cr_assert(code == 0);
+}
+
+Test(
+    graph_add_edge,
+    adds_edge,
+    .init = setup,
+    .fini = teardown
+) {
+  int v = 0, w = 5, code;
+  Graph_AddEdge(graph, v, w);
+
+  AdjVertexIter iter;
+  int adjacentVertex;
+
+  iter = AdjVertexIter_Create(graph, v);
+  code = AdjVertexIter_GetNext(iter, &adjacentVertex);
+  cr_expect(code == 0, "AdjVertexIter_GetNext failed with %d", code);
+  cr_assert(adjacentVertex == w);
+  AdjVertexIter_Free(&iter);
+
+  iter = AdjVertexIter_Create(graph, w);
+  code = AdjVertexIter_GetNext(iter, &adjacentVertex);
+  cr_expect(code == 0, "AdjVertexIter_GetNext failed with %d", code);
+  cr_assert(adjacentVertex == v);
+  AdjVertexIter_Free(&iter);
+}
+
